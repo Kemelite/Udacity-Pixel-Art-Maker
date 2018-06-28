4a8eea2b79cb2d02ad4182b91096a2e38c8065af
Kemelite <kemelayefa.larry@queztglobal.com>
2018-06-28 15:09:17 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,40 @@
 // Select color input
+
+
 // Select size input
+const siz = document.getElementbyId('pixelCanvas');
+let fakh = $("#inputHeight");
+let fakw = $("#inputWidth");
+
 
 // When size is submitted by the user, call makeGrid()
+$('#submit').click(function(e) {
+	e.preventDefault();
+	makeGrid();
+});
+
+let color = $("#colorPicker"); 
+
 
 function makeGrid() {
 
-// Your code goes here!
+	siz.innerHTML = '';
+	let height = fakh.val();
+	let width = fahw.val();
 
+
+	let addEvent = function(box) {
+		box.addEventListener('click', function() {
+			box.style.backgroundColor = color.val();
+		});
+	}
+
+for (let j = 0; j < height; j++) {
+	let row = siz.insertRow(j);
+	for (let m = 0; m < width; m++) {
+		let box = row.insertBox(m);
+	box.addEventListener('click', addEvent(box));
+	 }
+  }
+// Your code goes here!
 }
